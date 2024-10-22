9cfc3bae592f8dde893513df5d5bff8704e48f82
Amir Hossein Peyvand <amirhosseinpeyvand2020@gmail.com>
2024-10-22 13:14:19 -0700

    improcing the performance of js code

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
-let saveInputBtn = document.getElementById("input-btn");
 let myLeads = [];
-let inputEl = document.getElementById("input-el");
+const inputEl = document.getElementById("input-el");
+const saveInputBtn = document.getElementById("input-btn");
+const ulEl = document.getElementById("ul-el");
 
 saveInputBtn.addEventListener("click", () => {
   myLeads.push(inputEl.value);
-  console.log(myLeads);
+  renderLeads();
 });
+
+function renderLeads() {
+  let listItems = "";
+  for (let i = 0; i < myLeads.length; i++)
+    listItems += `<li>${myLeads[i]}</li>`;
+  ulEl.innerHTML = listItems;
+}
